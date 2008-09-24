00912f2d46b5ab6115496b5c3969be6fb0827f57
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2008-09-24 09:55:27 +0000

    Bumped the version number to 2.55

--- modules/IO-Socket-INET6/lib/IO/Socket/INET6.pm
+++ modules/IO-Socket-INET6/lib/IO/Socket/INET6.pm
@@ -27,7 +27,7 @@ use Exporter;
 use Errno;
 
 @ISA = qw(IO::Socket);
-$VERSION = "2.54";
+$VERSION = "2.55";
 #Purpose: allow protocol independent protocol and original interface.
 
 my $EINVAL = exists(&Errno::EINVAL) ? Errno::EINVAL() : 1;
