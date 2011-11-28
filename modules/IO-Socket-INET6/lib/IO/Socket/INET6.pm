b0abe84ac1344c74762806165f788fa24384cb31
Shlomi Fish <shlomif@iglu.org.il>
2011-11-28 14:56:29 +0200

    Bump the version number for the new release.

--- modules/IO-Socket-INET6/lib/IO/Socket/INET6.pm
+++ modules/IO-Socket-INET6/lib/IO/Socket/INET6.pm
@@ -41,7 +41,7 @@ use Carp;
 use Errno;
 
 @ISA = qw(IO::Socket);
-$VERSION = "2.68";
+$VERSION = "2.69";
 #Purpose: allow protocol independent protocol and original interface.
 
 my $EINVAL = exists(&Errno::EINVAL) ? Errno::EINVAL() : 1;
